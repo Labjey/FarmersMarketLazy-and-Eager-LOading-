b45a8b62510bf146208fb54ddbb35a0961e3f79c
Temesgen Habte <34597669+Labjey@users.noreply.github.com>
2018-07-12 16:47:00 -0500

    Add files via upload

---
+++ eagerLoading/app/app.component.ts
@@ -0,0 +1,14 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <a [routerLink]="['farmDetails']">Famers Market</a> 
+    <router-outlet></router-outlet>
+    `,
+  styles: []
+})
+export class AppComponent {
+  title = 'app';
+}
---
+++ eagerLoading/app/farmers-market/error.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error',
+  template: `
+    <p>
+      Error! Invalid URL
+    </p>
+  `,
+  styles: []
+})
+export class ErrorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
